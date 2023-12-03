b32c61ed397af6ce132137bdf181c4bf18942eec
Kevin Hernandez <k.hernandezt@uniandes.edu.co>
2023-12-03 15:10:36 -0500

    Modificaciones en el modelo de ServicioBasico y especificos

---
+++ parranderos/src/main/java/uniandes/edu/co/parranderos/modelo/ServicioBasico.java
@@ -0,0 +1,96 @@
+package uniandes.edu.co.parranderos.modelo;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "ServicioBasico")
+public class ServicioBasico {
+    @Id
+    private ObjectId _id;
+
+    private String id;
+
+    private String nombre;
+    private int capacidad;
+    private double costo;
+    private String horaApertura;
+    private String horaCierre;
+    private int[] consumos; 
+    private int servicioEspecifico;
+
+    public ServicioBasico(){}
+
+    public ServicioBasico(String id, String nombre, int capacidad, double costo, String horaApertura, String horaCierre) {
+        this.id = id;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.costo = costo;
+        this.horaApertura = horaApertura;
+        this.horaCierre = horaCierre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public String getHoraApertura() {
+        return horaApertura;
+    }
+
+    public void setHoraApertura(String horaApertura) {
+        this.horaApertura = horaApertura;
+    }
+
+    public String getHoraCierre() {
+        return horaCierre;
+    }
+
+    public void setHoraCierre(String horaCierre) {
+        this.horaCierre = horaCierre;
+    }
+
+    public int[] getConsumos() {
+        return consumos;
+    }
+
+    public void setConsumos(int[] consumos) {
+        this.consumos = consumos;
+    }
+
+    public int getServicioEspecifico() {
+        return servicioEspecifico;
+    }
+
+    public void setServicioEspecifico(int servicioEspecifico) {
+        this.servicioEspecifico = servicioEspecifico;
+    }
+}
